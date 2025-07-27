28b1c27eafdfd38d8ef85d5e1fdee69ba0c1a7fc
bunny <satyabanni11851@gmail.com>
2025-07-27 21:00:12 +0530

    updated controller to manage exception

--- Spring_CRM/src/main/java/com/dl/controller/LeadController.java
+++ Spring_CRM/src/main/java/com/dl/controller/LeadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/leads")
 public class LeadController {
