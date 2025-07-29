caf083f49f7bcfb266af613816ba9706e2348f72
bunny <satyabanni11851@gmail.com>
2025-07-29 20:40:28 +0530

    Spring security

---
+++ Spring_Security/src/main/java/com/dl/security/SpringCrm1Application.java
@@ -0,0 +1,13 @@
+package com.dl.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrm1Application.class, args);
+	}
+
+}
