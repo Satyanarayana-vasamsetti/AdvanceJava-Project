081fe3fb49d452cf79362548dc7e67961e3a7133
bunny <satyabanni11851@gmail.com>
2025-08-03 21:03:00 +0530

    added no argument constructor

--- SpringSecurity/src/main/java/com/dl/security/model/AppUser.java
+++ SpringSecurity/src/main/java/com/dl/security/model/AppUser.java
@@ -4,11 +4,19 @@ package com.dl.security.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 
 @Entity
 @Table(name = "app_user")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppUser {
 	
 	@Id
@@ -19,33 +27,4 @@ public class AppUser {
 	
 
 	private String password;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public AppUser(int id, String username, String password) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
-	}
-	
 }
