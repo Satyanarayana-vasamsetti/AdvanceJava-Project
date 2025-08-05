7b52919ac87bf24944ef682a2a87cab267f8782c
bunny <satyabanni11851@gmail.com>
2025-08-05 10:49:53 +0530

    JWT validation done

--- SpringSecurity/src/main/java/com/dl/security/service/JWTService.java
+++ SpringSecurity/src/main/java/com/dl/security/service/JWTService.java
@@ -1,17 +1,19 @@
 package com.dl.security.service;
 
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -40,9 +42,41 @@ public class JWTService {
 				.compact();
 	}
 
-	private Key getKey() {
+	private SecretKey getKey() {
 		byte[] keybytes = Decoders.BASE64.decode(secretkey);
 		return Keys.hmacShaKeyFor(keybytes);
 	}
+
+	public String extractUserName(String token) {
+		// TODO Auto-generated method stub
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	 private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+	        final Claims claims = extractAllClaims(token);
+	        return claimResolver.apply(claims);
+	    }
+
+	    private Claims extractAllClaims(String token) {
+	        return Jwts.parser()
+	                .verifyWith(getKey())
+	                .build()
+	                .parseSignedClaims(token)
+	                .getPayload();
+	    }
+
+	    public boolean validateToken(String token, UserDetails userDetails) {
+	        final String userName = extractUserName(token);
+	        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	    }
+
+	    private boolean isTokenExpired(String token) {
+	        return extractExpiration(token).before(new Date());
+	    }
+
+	    private Date extractExpiration(String token) {
+	        return extractClaim(token, Claims::getExpiration);
+	    }
+
 
 }
